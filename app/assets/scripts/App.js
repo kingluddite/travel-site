b6f7f80c69b29edd96f8244cb70740f3a7466dfb
PEH2 <howley.phil@gmail.com>
2017-06-24 13:29:36 -0700

    Complete modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,7 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import HighlightSection from './modules/HighlightSection';
 import SmoothScrolling from './modules/SmoothScrolling';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
@@ -11,3 +12,4 @@ new RevealOnScroll($('.testimonial'), '60%');
 const stickyHeader = new StickyHeader();
 const highlightSection = new HighlightSection();
 const smoothScrolling = new SmoothScrolling();
+const modal = new Modal();
